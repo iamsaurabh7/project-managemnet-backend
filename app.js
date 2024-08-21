da20708a7736a9f22e9d23478ca7880c0f961d54
SAURABH VERMA <iamsaurabhverma7@gmail.com>
2024-08-21 19:39:28 +0530

    cors update

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-var cors = require('cors')
-const projectRoutes = require('./routes/projectRoutes');
-// const authRoutes = require('./routes/authRoutes'); 
+const express = require("express");
+const bodyParser = require("body-parser");
+var cors = require("cors");
+const projectRoutes = require("./routes/projectRoutes");
+// const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-app.use(cors())
+app.use(
+  cors({
+    origin: "https://project-management-app-demo.netlify.app/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(bodyParser.json());
 
 // Routes
-app.get("/",(req,res)=>{
-    res.send("hello")
-})
-app.use('/api/projects', projectRoutes);
-// app.use('/api/auth', authRoutes);  
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+app.use("/api/projects", projectRoutes);
+// app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 8080;
 
